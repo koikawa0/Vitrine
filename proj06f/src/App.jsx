6768a84f423f77bc3e8a7f28f6d2a02cb5c39ebd
Koikawa <kaiofittipaldisc@gmail.com>
2025-06-25 18:49:30 -0300

    first commit

---
+++ proj06f/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Vitrine from "./pages/Vitrine.jsx"
+
+export default function Rotas() {
+  return <BrowserRouter>
+    <Routes>
+      <Route index path="/" element={<Vitrine/>}/>
+    </Routes>
+  </BrowserRouter>
+}
---
+++ proj06f/src/datas/ProdutosExemplo.js
@@ -0,0 +1,16 @@
+const ProdutosExemplo = [
+  {
+      codigo: "1234",
+      marca: "Marca",
+      modelo: "Modelo1",
+      preco: 1000,
+      descricao: "O produto é muito bonito porque...",
+      imagens: [
+          "https://foto.com/produto-imagem-1.jpg",
+          "https://foto.com/produto-imagem-2.jpg",
+          "https://foto.com/produto-imagem-3.jpg",
+      ]
+  },
+  /.../
+]
+export default ProdutosExemplo
