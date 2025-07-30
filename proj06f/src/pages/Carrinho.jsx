3b143dd42fe9cd96fbb079b07ea5ee249cd0d8b2
koikawa0 <kaiofittipaldisc@gmail.com>
2025-07-30 18:59:54 -0300

    carrinho1

--- proj06f/src/pages/Carrinho.jsx
+++ proj06f/src/pages/Carrinho.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navegacao from "../components/Navegacao";
 import ProdutosExemplo from "../datas/ProdutosExemplo";
 import Janela from "../components/Janela";
+import ObterCarrinho from "../functions/obterCarrinho";
 
 export default function Carrinho() {
+  const [carrinho, definirCarrinho] = useState([]);
+  const [preco, definirPreco] = useState(0);
+
+  useEffect(function () {
+    const resultado = ObterCarrinho();
+    definirCarrinho(resultado);
+  }, []);
+
+  useEffect(
+    function () {
+      var total = 0;
+      carrinho.map(function (codigo) {
+        for (const produto of ProdutosExemplo)
+          if (produto.codigo == codigo) total += parseInt(produto.preco);
+      });
+      definirPreco(total);
+    },
+    [carrinho]
+  );
+
+  const linhas = carrinho.map(function (codigo, indice) {
+    for (const produto of ProdutosExemplo) {
+      if (produto.codigo == codigo)
+        return (
+          <tr key={indice}>
+            <td>{produto.codigo}</td>
+            <td>{produto.modelo}</td>
+            <td>R$ {produto.preco}.00</td>
+          </tr>
+        );
+    }
+  });
+
   return (
     <>
       <Navegacao titulo="VITRINE">
@@ -14,14 +48,22 @@ export default function Carrinho() {
 
       <Janela>
         <table width="100%">
-            <tbody>
-                <tr>
-                    <td>Código</td>
-                    <td>Modelo do Produto</td>
-                    <td>R$ 1.000,00</td>
-                </tr>
-            </tbody>
+          <tbody>
+            <tr>
+              <td>
+                <b>Código</b>
+              </td>
+              <td>
+                <b>Modelo do Produto</b>
+              </td>
+              <td>
+                <b>Preço</b>
+              </td>
+            </tr>
+            {linhas}
+          </tbody>
         </table>
+        <h1>Total R$ {preco},00</h1>
       </Janela>
     </>
   );
